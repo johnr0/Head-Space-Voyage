c154dfe33f899e42796c339d9366144875ab1ad5
안정건 <>
2018-03-24 16:56:15 +0900

    장애물

---
+++ Assets/Script/Obstacles/Base/Obstacle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Obstacle : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public Rigidbody Rigidbody
+    {
+        get
+        {
+            if (_rigidbody == null)
+                _rigidbody = GetComponent<Rigidbody>();
+            return _rigidbody;
+        }
+    }
+
+    public virtual void ChangeMass(float mass)
+    {
+        Rigidbody.mass = mass;
+    }
+
+    public virtual void ChangeVelocity(Vector2 vel)
+    {
+        Rigidbody.velocity = new Vector3(vel.x, vel.y, 0f);
+    }
+}
