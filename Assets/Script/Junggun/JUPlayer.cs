d08df268fe3073a363af42f3026a0fdd455c260a
안정건 <>
2018-04-07 19:58:44 +0900

    정건조작

---
+++ Assets/Script/Junggun/JUPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JUUtil;
+
+public class JUPlayer : Singleton<JUPlayer> {
+
+    private Transform _bodyTransform;
+    public Transform bodyTransform
+    {
+        get
+        {
+            if (_bodyTransform == null)
+                _bodyTransform = transform.GetChild(0);
+            return _bodyTransform;
+        }
+    }
+    private Vector3 relativeBodyPosition;
+    private Rigidbody _prigidbody;
+    public Rigidbody prigidbody
+    {
+        get
+        {
+            if (_prigidbody == null)
+                _prigidbody = GetComponent<Rigidbody>();
+            return _prigidbody;
+        }
+    }
+    [SerializeField]
+    private float Power;
+    private EPlayerState CurrentState = EPlayerState.Inputable;
+
+
+	private void Awake()
+	{
+        Init();
+	}
+
+	public void Init()
+    {
+        relativeBodyPosition = bodyTransform.localPosition;
+    }
+
+	private void Update()
+	{
+        if (Input.GetButtonDown("Eject"))
+            Eject();
+	}
+
+	private void FixedUpdate()
+	{
+        Debug.LogFormat("CurrentVelocity is : {0}. Current Speed is : {1}", prigidbody.velocity, prigidbody.velocity.magnitude);
+	}
+
+	public void Eject()
+    {
+        if (CurrentState != EPlayerState.Inputable)
+            return;
+        bodyTransform.parent = null;
+        prigidbody.AddForce(Power * transform.forward, ForceMode.Impulse);
+        CurrentState = EPlayerState.NotInputable;
+    }
+
+    public void ReloadBody()
+    {
+        bodyTransform.parent = transform;
+        bodyTransform.localPosition = relativeBodyPosition;
+    }
+
+}
---
+++ Assets/Script/Junggun/JUUtil.cs
@@ -0,0 +1,9 @@
+﻿namespace JUUtil
+{
+    public enum EPlayerState
+    {
+        Inputable = 0,
+        NotInputable = 1,
+    }
+}
+
