8c1b66cfa6d768649109a8a46f1dd925b2e76c26
johnr0 <johnr0hol@gmail.com>
2018-04-07 20:50:15 +0900

    keyboard based head

--- Assets/Script/Character/Character_movement_body.cs
+++ Assets/Script/Character/Character_movement_body.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Character_movement_body : MonoBehaviour {
     private float spin_vel;
-    private float max_spin_vel;
-    private float max_thrust_vel;
+    public float max_spin_vel;
+    public float max_thrust_vel;
     private float thrust_vel;
     private Rigidbody rbody;
-	private float cur_weight = 3f;
+	public float cur_weight = 3f;
 	private float min_weight = 1f;
+	private bool r_input;
+	private bool l_input;
+	private bool space_input;
+	private Transform body;
 	// Use this for initialization
 	void Start () {
 		cur_weight = 3f;
@@ -18,26 +22,29 @@ public class Character_movement_body : MonoBehaviour {
 		max_thrust_vel = 15f/cur_weight;
         thrust_vel = 0f;
         rbody = GetComponent<Rigidbody>();
+		body = transform.Find ("body");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		space_input = Input.GetButton ("head_eject");
+		r_input = Input.GetButton ("right_thrust");
+		l_input = Input.GetButton ("left_thrust");
+		Debug.LogFormat ("Ej : {0}, RT : {1} , LT : {2}", Input.GetButton ("head_eject")
+			, Input.GetButton ("right_thrust")
+			, Input.GetButton ("left_thrust"));
     }
 
 	void FixedUpdate(){
-		if (Input.GetButton ("head_eject")) {
-			print ("eject");
-			head_eject ();
-		} else {
-			print ("hey");
-			bool r_input = Input.GetButton ("right_thrust");
-			bool l_input = Input.GetButton ("left_thrust");
-			rbody.angularVelocity = 5f / Time.deltaTime / cur_weight * new Vector3 (0, 0, Add_Spin (r_input, l_input));
-			spin_vel = rbody.angularVelocity.z;
-			rbody.velocity += 5f / cur_weight * transform.up * Add_Thrust (r_input, l_input);
-			thrust_vel = rbody.velocity.magnitude;
-		}
+
+		rbody.angularVelocity = 5f / Time.deltaTime / cur_weight * new Vector3 (0, 0, Add_Spin (r_input, l_input));
+		spin_vel = rbody.angularVelocity.z;
+		rbody.velocity += 5f / cur_weight * transform.up * Add_Thrust (r_input, l_input);
+		thrust_vel = rbody.velocity.magnitude;
+
+
+		head_eject ();
+
 	}
 
     private float Add_Spin(bool r_input, bool l_input)
@@ -126,8 +133,20 @@ public class Character_movement_body : MonoBehaviour {
         }
     }
 	private void head_eject(){
-		cur_weight = 1f;
-		rbody.velocity = transform.up * 15f;
+		if (space_input && cur_weight != 1f) {
+			//set the velocity 
+			cur_weight = 1f;
+			rbody.velocity = transform.up * 15f;
+			max_thrust_vel = 15f/cur_weight;
+			max_spin_vel = 15f/cur_weight;
+
+			//get rid of the body
+			body.gameObject.AddComponent<Rigidbody> ();
+			body.GetComponent<Rigidbody> ().velocity = -1f * transform.up;
+			body.GetComponent<Rigidbody> ().useGravity = false;
+			body.parent = null;
+			body = null;
+		}
 	}
 
 }
